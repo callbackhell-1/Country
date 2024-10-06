75759e3dc409a20907a7ccc2be03c632fb1e14aa
Adarsh <jha01adarsh@gmail.com>
2024-10-06 21:08:43 +0530

    project setup

---
+++ script.js
@@ -0,0 +1,7 @@
+import react from 'react';
+import reactDOM from 'react-dom/client';
+import App from './App';
+
+const root = reactDOM.createRoot(document.querySelector('#root'));
+
+root.render(<App />);
